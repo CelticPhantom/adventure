5e601ec4547e8fb3bc140e8702d4f0ed39fa9ccb
Fintan <waycontech@gmail.com>
2018-01-10 15:48:03 +0000

    fixes #1: check if user has entered a valid direction
    Challenges:
    Added a Map
    Added a description for each place
    Added stuff in each location that can be taken

--- data.py
+++ data.py
@@ -4,3 +4,17 @@ locations = {
     (1, 0): 'park',
     (1, 1): 'market'
 }
+
+placeDescriptions = {
+    'house' : 'A large mansion',
+    'lake' : 'A body of water',
+    'park' : 'Green fields and trees',
+    'market' : 'A bazaar'
+}
+
+placeObjects = {
+    'house' : ['Television', 'Couch', 'Bed'],
+    'lake' : ['Boat', 'Sail', 'Canoe'],
+    'park' : ['Picnic Basket', 'Football', 'Kite'],
+    'market' : ['Apple', 'Diamond Necklace', 'Painting']
+}
